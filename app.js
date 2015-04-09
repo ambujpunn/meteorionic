e637450f3e2c651860c1420d3c2b83daf91baf11
Ambuj Punn <ambujpunn@gmail.com>
2015-04-08 23:15:08 -0700

    updated to include cordova integration with angular

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ if (Meteor.isClient) {
         'angular-meteor',
         'ui.router',
         'ionic']);
+
+    // to get meteor app on cordova with angular integration
+    function onReady() {
+      angular.bootstrap(document, ['starter']);
+    }
+
+    if (Meteor.isCordova)
+      angular.element(document).on("deviceready", onReady);
+    else
+      angular.element(document).ready(onReady);
+
     var test = 5;
     app.controller('StoreController', function(){
         this.prod = test;
